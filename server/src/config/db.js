4f431a803105e208199d1b765b6a8943fbed74f8
Hannah Davantes <hannahdavantes@yahoo.com>
2026-04-16 19:21:28 -0600

    fix db config for azure

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -8,10 +8,9 @@ const dbConfig = {
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT, 10),
   options: {
-    encrypt: false,
-    trustServerCertificate: true,
+    encrypt: true,
+    trustServerCertificate: false,
   },
 };
 
